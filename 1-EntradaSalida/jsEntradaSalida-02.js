c8ca1397ab4f3761ff3eab2ae5ea0baeb0e0ce21
mario <mariorampi@gmail.com>
2023-01-30 12:25:55 -0300

    Guia 1, ej 1  2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,17 @@
 /*
-Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+	Debemos lograr tomar un nombre con 'prompt' 
+	y luego mostrarlo por 'alert' al presionar el botón  'mostrar'
+*/
 function mostrar()
 {
-	alert("ok");
-}
+	// alert("ok");
+	
+	// var edad;
+	let nombre;
+	let nombreCompleto; // Notacion Lower camel case
+	// nombre = "Mario"; // Asignacion de string.
+	nombre = prompt("Ingrese un nombre");
 
+	// Mostrar por alert
+	alert(nombre);
+}
